5202e897054f5e5c4e053a79409a3c5b7d25e482
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-09-16 11:30:14 +0300

    task 3: creating base model class

--- models/base_model.py
+++ models/base_model.py
@@ -1,109 +1,58 @@
-#!/usr/bin/python3
-"""Module with a class"""
-import uuid
+"""Module with class"""
+
+from uuid import uuid4
 from datetime import datetime
 
 
 class BaseModel:
     """
-    Class that defines all common attributes/methods for other classes.
+    BaseModel class:
+    Class that defines all common attributes/methods
+    for other classes.
     """
-    # self stands for the instance itself
 
-    def __init__(self, *args, **kwargs):
-        """
-        Defines public instance attributes:
-            id: (string) That's assigned a unique id.
-            created_at: (datetime) That's assigned the current date
-                        when an instance is created.
-            updated_at: (datetime) That's assigned the the current datetime
-                        when an instance is created and it will be updated
-                        every time you change your object.
-        """
-        # each key of the kwargs dictionay is an attribute name, except for
-        # __class__ that shouldn't be added as an attribute.
-        # each value of the dict is an attribute name.
-        if len(kwargs) != 0:
-            for key, value in kwargs.items():
-                # created_at is a str in kwargs dic, change to datetime format
-                if key == 'created_at':
-                    if type('created_at') == str:
-                        value = datetime.strptime(
-                            value, '%Y-%m-%dT%H:%M:%S.%f')
-
-                # updated_at is a str in kwargs dic, change to datetime format
-                elif key == 'updated_at':
-                    if type('updated_at') == str:
-                        value = datetime.strptime(
-                            value, '%Y-%m-%dT%H:%M:%S.%f')
-
-                # anything else that's not attribute like __class__
-                self.__dict__[key] = value
-
-                # or you can say if key != '__class__', setarr(self, key, value)
-
-        else:
-            # if no kwargs then generate you own
-            # id should str when saved in __dict__, can not wait until to_dict()
-            # runs for id to be string :)
-            self.id = str(uuid.uuid4())
-
-            # .now() picks up the current time when instance is intialised
-
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+    # Public Attributes
+    id = str(uuid4())  # creates unique id for each basemodel
+    created_at = datetime.now()  # time of when instance created
+    updated_at = datetime.now()
 
     def __str__(self):
-        """Returns the information about an instance/object like:
-            Class name,
-            Instance id,
-            Dictionary of the attributes of instance
         """
-
-        # for the name of class use -> self.__class__.__name__
-        """self.__dict__ is a dictionary that stores
-            an instances's writable attributes"""
-
+        prints the class name,
+        the id,
+        and dictionery of the attributes for instance
+        """
         info = "[{}] ({}) {}".format(
             self.__class__.__name__, self.id, self.__dict__)
         return info
 
-    # Pubic instance methods:
+    # Public methods
 
     def save(self):
         """
-        Updates the public instance attribute updated_at
-        with the current datetime when changes are done.
+        updates the public instance attribute updated_at
+        with the current datetime
         """
-
         self.updated_at = datetime.now()
 
     def to_dict(self):
         """
-        Generate a dictionary representation of an instance. 
-        Returns a dictionary containing all keys/values of __dict__
-        of the instance. Plus the class name, created_at and updated_at
-        as string object in ISO format. 
+        returns a dictionary containing all
+        keys/values of __dict__ of the instance
+
+        This method be the first piece of the
+        serialization/deserialization process:
         """
 
+        # Note: using self.__dict__,
+        # only instance attributes set will be returned
+        # so now we want dictionery of every info
+
         dic = {}
-        # we need everything of __dic__ in dic, so we just assign it to dic
+
+        # we also need what is in __dict__ to be inside dic
         dic = self.__dict__
-
-        # then we add the rest info
-        # note: keys have to be strings
-        dic['__class__'] = self.__class__.__name__
-
-        # No need to add id because it's in __dict__
-        #dic['id'] = id
-
-        # change created_at & updated_at to ISO format
-
-        created_at = str(self.created_at.isoformat())
-        updated_at = str(self.updated_at.isoformat())
-
-        # add them to dic plus the id
-        dic['created_at'] = created_at
-        dic['updated_at'] = updated_at
-
+        dic["__class__"] = self.__class__.__name__
+        dic["created_at"] = str(self.created_at.isoformat())
+        dic["updated_at"] = str(self.updated_at.isoformat())
         return dic
